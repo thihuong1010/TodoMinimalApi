67cf4163cb8d8331971c657c1187cd70a9b3c4c1
Huong Nguyen <65859562+thihuong1010@users.noreply.github.com>
2023-08-07 22:03:09 +0300

    Defines db context

---
+++ TodoMinimalApi/TodoDb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoMinimalApi
+{
+    class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
+
