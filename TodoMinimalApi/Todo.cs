3b51cd32673b688c00c6ea4ce0be313b9eddcf26
Huong Nguyen <65859562+thihuong1010@users.noreply.github.com>
2023-08-07 22:00:34 +0300

    Create the model for the app
    - A model is a class that represents data that the app manages

---
+++ TodoMinimalApi/Todo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TodoMinimalApi
+{
+	public class Todo
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public bool IsComplete { get; set; }
+	}
+}
+
